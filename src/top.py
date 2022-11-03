f89c20ee7ac6440e6f7ba4ed4dc7e00a8c940807
burgess01 <burgess01@allegheny.edu>
2022-11-02 21:08:32 -0400

    docs: correct formatting in main

--- src/top.py
+++ src/top.py
@@ -19,13 +19,25 @@ def process_stats():
     pass
 
 
-def top():
+def top(interactMode):
     """Function to compile all diagnostic information into a organized display."""
+
     pass
 
 
-def main(interact: bool):
-    pass
+def main(
+    nonInteract: bool = typer.Option(
+        None,
+        "--non-interactive",
+        help="if you want the program to run only one time or iteratively.",
+    )
+):
+    if nonInteract is not None:
+        # if they want to run it more than once:
+        top(nonInteract)
+    else:
+        # run normally through schedule
+        top(nonInteract)
 
 
 if __name__ == "__main__":
