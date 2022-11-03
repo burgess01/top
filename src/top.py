fc1f087fbe0f1ba1f69f7452029e7a5aeadaa82a
burgess01 <burgess01@allegheny.edu>
2022-11-02 20:52:20 -0400

    feature: add imports

--- src/top.py
+++ src/top.py
@@ -1,18 +1,24 @@
 """ Program in order to create a 'top' command implemented in Python. """
 
+import schedule
+import psutil
+from datetime import datetime
+import pandas as pd
+import os
+
 
 def upper_diagnostics():
-    """ """
+    """Function to calculate all 'upper' diagnostics."""
     pass
 
 
 def process_stats():
-    """ """
+    """Function to calculate order of processes in top command."""
     pass
 
 
 def top():
-    """ """
+    """Function to compile all diagnostic information into a organized display."""
     pass
 
 
