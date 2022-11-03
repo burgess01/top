b608140170d78bb72fd0bc040ccc5dc866d72a53
burgess01 <burgess01@allegheny.edu>
2022-11-02 20:49:31 -0400

    docs: create outline of code structure

---
+++ src/top.py
@@ -0,0 +1,24 @@
+""" Program in order to create a 'top' command implemented in Python. """
+
+
+def upper_diagnostics():
+    """ """
+    pass
+
+
+def process_stats():
+    """ """
+    pass
+
+
+def top():
+    """ """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
