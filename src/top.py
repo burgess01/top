df5e9009c3dc8ea656cca213cdbfdbb9f94f7145
burgess01 <burgess01@allegheny.edu>
2022-11-04 11:31:20 -0400

    feature: add preliminary top formatting function

--- src/top.py
+++ src/top.py
@@ -91,8 +91,46 @@ def process_stats():
 
 def top():
     """Function to compile all diagnostic information into a organized display."""
-    upper_diagnostics()
-    pass
+    (
+        processes,
+        load_averages,
+        cpu_usage,
+        overall_usage,
+        vm,
+        network_info,
+        disk_info,
+    ) = upper_diagnostics()
+
+    processes = process_stats()
+    newnow = datetime.now()
+    # add user - readable version, change bit values to GB values
+    # exec call , sys package, shell = True; look into textual for better option
+    print(
+        f"Processes: {len(processes)} total, _ running, _ sleeping, _ threads",
+        "{:>25}".format(newnow.strftime("%H:%M:%S")),
+    )
+    print(
+        f"Load Avg: {round(load_averages[0], 2)}, {round(load_averages[1], 2)}, {round(load_averages[2], 2)}  CPU usage: {round((cpu_usage[0]/overall_usage) * 100, 2)}% user, {round((cpu_usage[2]/overall_usage) * 100,2)}% sys, {round((cpu_usage[3]/overall_usage) * 100,2)}% idle"
+    )
+    print(f"SharedLibs: _ resident, _ data, _ linkedit.")
+    print(f"MemRegions: _ total, _ resident, _ private, _ shared.")
+    print(f"PhysMem: _ used (_ wired), _ unused.")
+    print(
+        f"VM: {vm[0]} vsize, {vm[7]} wired, {vm[2]} available, {vm[4]} active, {vm[5]} inactive."
+    )
+    print(
+        f"Networks: packets: {network_info[3]}/{network_info[3] / (1024.0 ** 3)}G in, {network_info[2]}/{network_info[2] / (1024.0 ** 3)}G out."
+    )
+    print(
+        f"Disks: {disk_info[0]}/{disk_info[0] >> 20}G read, {disk_info[1]}/{disk_info[1] >> 20}G written"
+    )
+
+    # lower section print
+    print("PID         COMMAND        CPU%           TIME             STATUS")
+    for i in range(15):
+        proc = processes[i]
+        print("{: <10} {: <15} {: <10} {: <20} {: <20}".format(*proc))
+
 
 
 def main(
