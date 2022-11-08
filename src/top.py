cf09c74270720ab30d3258bf9e756ef94a576397
burgess01 <burgess01@allegheny.edu>
2022-11-08 14:54:36 -0500

    feature: refactor formatting in overall output

--- src/top.py
+++ src/top.py
@@ -137,6 +137,10 @@ def top():
         load_averages,
         cpu_usage,
         overall_usage,
+        cpu_count,
+        phys_cpus,
+        numUsers,
+        bootTime,
         vm,
         totalvm,
         swap,
@@ -144,21 +148,22 @@ def top():
         disk_info,
     ) = upper_diagnostics()
 
-    processes, sleeping, running = process_stats()
+    processes, sleeping, running, threads = process_stats()
     newnow = datetime.now()
     # add user - readable version, change bit values to GB values
     # exec call , sys package, shell = True; look into textual for better option
     print(
-        f"Processes: {len(processes)} total, {running} running, {sleeping} sleeping, _ threads",
-        "{:>23}".format(newnow.strftime("%H:%M:%S")),
+        f"Processes: {len(processes)} total, {running} running, {sleeping} sleeping, {threads} threads",
+        "{:>20}".format(newnow.strftime("%H:%M:%S")),
     )
     print(
         f"Load Avg: {round(load_averages[0], 2)}, {round(load_averages[1], 2)}, {round(load_averages[2], 2)}  CPU usage: {round((cpu_usage[0]/overall_usage) * 100, 2)}% user, {round((cpu_usage[2]/overall_usage) * 100,2)}% sys, {round((cpu_usage[3]/overall_usage) * 100,2)}% idle"
     )
-    print(f"SharedLibs: _ resident, _ data, _ linkedit.")
-    print(f"MemRegions: _ total, _ resident, _ private, _ shared.")
+    print(
+        f"CPU counts: {cpu_count} total, {phys_cpus} physical  Users: {numUsers}  Boot Time: {bootTime}"
+    )
     print(f"PhysMem: {size(vm[0])} used, {size(vm[7])} wired, {vm[5]} inactive.")
-    print(f"VM: {totalvm} vsize, {swap[4]}(0) swapins, {swap[5]}(0) swapouts")
+    print(f"VM: {size(totalvm)} vsize, {swap[4]}(0) swapins, {swap[5]}(0) swapouts")
     print(
         f"Networks: packets: {network_info[3]}/{size(network_info[3])} in, {network_info[2]}/{size(network_info[2])} out."
     )
@@ -167,10 +172,10 @@ def top():
     )
 
     # lower section print
-    print("PID         COMMAND        CPU%           TIME             STATUS")
+    print("\nPID       COMMAND    CPU%   TIME       USER     STATUS")
     for i in range(14):
         proc = processes[i]
-        print("{: <10} {: <15} {: <10} {: <20} {: <20}".format(*proc))
+        print("{: <7} {: <12} {: <5} {: <8} {: <11} {: <10}".format(*proc))
 
 
 def main():
